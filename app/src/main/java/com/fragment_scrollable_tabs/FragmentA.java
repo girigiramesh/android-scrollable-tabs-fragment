89993ad68d9f72b2af609fc8483dbb53e0027421
ramesh <hari.kvana@gmail.com>
2016-09-08 13:05:09 +0530

    pagetitle is added

--- app/src/main/java/com/fragment_scrollable_tabs/FragmentA.java
+++ app/src/main/java/com/fragment_scrollable_tabs/FragmentA.java
@@ -1,8 +1,10 @@
 package com.fragment_scrollable_tabs;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,83 @@ import android.view.ViewGroup;
  * Created by Ramesh on 9/8/16.
  */
 public class FragmentA extends Fragment {
+    private static final String TAG = ".FragmentA";
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Log.d(TAG, "onAttach:onattach ");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState == null){
+            Log.d(TAG, "onCreate: First time");
+        }else{
+            Log.d(TAG, "onCreate: subsequent time");
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater,container,savedInstanceState);
+        Log.d(TAG, "onCreateView: oncreateview");
         return inflater.inflate(R.layout.fragment_a,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated: onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: onPause");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: onSaveInstanceState");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView: onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach: onDetach");
     }
 }
--- app/src/main/java/com/fragment_scrollable_tabs/MainActivity.java
+++ app/src/main/java/com/fragment_scrollable_tabs/MainActivity.java
@@ -1,16 +1,15 @@
 package com.fragment_scrollable_tabs;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends FragmentActivity {
     ViewPager viewPager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +19,8 @@ public class MainActivity extends FragmentActivity {
         viewPager.setAdapter(new MyAdapter(fragmentManager));
     }
 }
-class MyAdapter extends FragmentPagerAdapter{
+
+class MyAdapter extends FragmentStatePagerAdapter {
 
     private static final String TAG = ".MainActivity";
 
@@ -30,15 +30,15 @@ class MyAdapter extends FragmentPagerAdapter{
 
     @Override
     public Fragment getItem(int position) {
-        Log.d(TAG, "getItem: +position");
+//        Log.d(TAG, "getItem: +position");
         Fragment fragment = null;
-        if(position == 0){
+        if (position == 0) {
             fragment = new FragmentA();
         }
-        if(position == 1){
+        if (position == 1) {
             fragment = new FragmentB();
         }
-        if(position == 2){
+        if (position == 2) {
             fragment = new FragmentC();
         }
         return fragment;
@@ -46,7 +46,22 @@ class MyAdapter extends FragmentPagerAdapter{
 
     @Override
     public int getCount() {
-        Log.d(TAG, "get count is called");
+//        Log.d(TAG, "get count is called");
         return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = new String();
+        if(position == 0){
+            return "Tab 1";
+        }
+        if(position == 1){
+            return "Tab 2";
+        }
+        if(position == 2){
+            return "Tab 3";
+        }
+        return null;
     }
 }
